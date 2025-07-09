804acbc438ecb15d626c551091804f207f9b9158
romasmv <rmorozovas@gmail.com>
2025-07-09 12:45:20 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const kingBoocksEl = document.getElementById("king-books")
+
 fetch('https://stephen-king-api.onrender.com/api/books')
 .then(res => res.json())
-.then((data) => console.log())
+.then((data) => {
+    data.data.forEach(boock => {
+        kingBoocksEl.insertAdjacentHTML("beforeend",
+            `<tr>
+                <td>${boock.Title}</td>
+                <td>${boock.Year}</td>
+                <td>${boock.Publisher}</td>
+                <td>${boock.ISBN}</td>
+                <td>${boock.Pages}</td>
+                <td>${boock.Notes[0]}</td>
+            </tr>`)
+
+    })
+})
+.catch(error => console.log(error))
