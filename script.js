a78a74d36e2a23677ca84f063ea53f3971ef00f3
romasmv <rmorozovas@gmail.com>
2025-07-16 12:23:38 +0300

    up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const kingBoocksEl = document.getElementById("king-books")
+const kingBooksEl = document.getElementById("king-books")
 const loaderEl = document.getElementById("loader")
 const tableEl = document.getElementById("table")
 
@@ -12,15 +12,15 @@ fetch('https://stephen-king-api.onrender.com/api/books')
     tableEl.classList.remove("hide")
     loaderEl.classList.remove("show")
 
-    data.data.forEach(boock => {
-        kingBoocksEl.insertAdjacentHTML("beforeend",
-            `<tr data-bookid = "${boock.id}">
-                <td>${boock.Title}</td>
-                <td>${boock.Year}</td>
-                <td>${boock.Publisher}</td>
-                <td>${boock.ISBN}</td>
-                <td>${boock.Pages}</td>
-                <td>${boock.Notes[0] ? boock.Notes.join(' '):'No additional notes'}</td>
+    data.data.forEach(book => {
+        kingBooksEl.insertAdjacentHTML("beforeend",
+            `<tr data-bookid = "${book.id}">
+                <td>${book.Title}</td>
+                <td>${book.Year}</td>
+                <td>${book.Publisher}</td>
+                <td>${book.ISBN}</td>
+                <td>${book.Pages}</td>
+                <td>${book.Notes[0] ? book.Notes.join(' '):'No additional notes'}</td>
             </tr>`)
 
     })
@@ -28,10 +28,13 @@ fetch('https://stephen-king-api.onrender.com/api/books')
 .catch(error => console.log(error))
 
 
-kingBoocksEl.addEventListener("click", e => {
-    const villainsRowEl = document.getElementById('villains-row');
-    villainsRowEl && kingBoocksEl.removeChild(villainsRowEl);
+kingBooksEl.addEventListener("click", e => {
     const tr  =e.target.parentElement
+    const villainsRowEl = document.getElementById('villains-row');
+    if(villainsRowEl && villainsRowEl != tr){
+        kingBooksEl.removeChild(villainsRowEl)
+    }
+
     const bid = tr.dataset.bookid
     fetch('https://stephen-king-api.onrender.com/api/book/'+bid)
     .then(res => res.json())
@@ -40,9 +43,8 @@ kingBoocksEl.addEventListener("click", e => {
         tr.insertAdjacentHTML('afterend',
              `<tr id="villains-row">
                  <td colspan="2">${data.data.Title}</td>
-                 <td colspan="4">${data.data.villains}</td>
+                 <td colspan="4">${data.data.villains.map((villain) => villain.name).join("<br/>")}</td>
              </tr>`)
-    
     })
 
     .then((err) => console.log(err));
