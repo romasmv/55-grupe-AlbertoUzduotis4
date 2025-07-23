468cfc651232e575ee5d19e84505ea695b403780
romasmv <rmorozovas@gmail.com>
2025-07-23 11:47:46 +0300

    up

--- script.js
+++ script.js
@@ -1,53 +1,64 @@
-const kingBooksEl = document.getElementById("king-books")
-const loaderEl = document.getElementById("loader")
-const tableEl = document.getElementById("table")
+const kingBooksEl = document.getElementById("king-books");
+const loaderEl = document.getElementById("loader");
+const tableEl = document.getElementById("table");
 
-tableEl.classList.add("hide")
-loaderEl.classList.add("show")
+tableEl.classList.add("hide");
+loaderEl.classList.add("show");
 
-fetch('https://stephen-king-api.onrender.com/api/books')
-.then(res => res.json())
-.then((data) => {
-
-    tableEl.classList.remove("hide")
-    loaderEl.classList.remove("show")
-
-    data.data.forEach(book => {
-        kingBooksEl.insertAdjacentHTML("beforeend",
-            `<tr data-bookid = "${book.id}">
+fetch("https://stephen-king-api.onrender.com/api/books")
+  .then((resp) => resp.json())
+  .then((data) => {
+    loaderEl.classList.remove("show");
+    tableEl.classList.remove("hide");
+    data.data.forEach((book) => {
+      kingBooksEl.insertAdjacentHTML(
+        "beforeend",
+        `<tr data-bookid="${book.id}">
                 <td>${book.Title}</td>
                 <td>${book.Year}</td>
                 <td>${book.Publisher}</td>
                 <td>${book.ISBN}</td>
                 <td>${book.Pages}</td>
-                <td>${book.Notes[0] ? book.Notes.join(' '):'No additional notes'}</td>
-            </tr>`)
+                <td>${
+                  book.Notes[0] ? book.Notes.join("; ") : "No additional notes"
+                }</td>
+            </tr>`
+      );
+    });
+  })
+  .catch((error) => console.log(error));
 
-    })
-})
-.catch(error => console.log(error))
-
-
-kingBooksEl.addEventListener("click", e => {
-    const tr  =e.target.parentElement
-    const villainsRowEl = document.getElementById('villains-row');
-    
-    if(villainsRowEl && villainsRowEl != tr){
-        kingBooksEl.removeChild(villainsRowEl)
-    }
-    if(villainsRowEl != tr){
-    const bid = tr.dataset.bookid
-    fetch('https://stephen-king-api.onrender.com/api/book/'+bid)
-    .then(res => res.json())
-    .then((data) => {
-        console.log(data)
-        tr.insertAdjacentHTML('afterend',
-             `<tr id="villains-row">
-                 <td colspan="2">${data.data.Title}</td>
-                 <td colspan="4">${data.data.villains.map((villain) => villain.name).join("<br/>")}</td>
-             </tr>`)
-    })
-
-    .then((err) => console.log(err));
+kingBooksEl.addEventListener("click", (e) => {
+  const tr = e.target.parentElement;
+  const villainsRowEl = document.getElementById("villains-row");
+  if (villainsRowEl && villainsRowEl != tr) {
+    kingBooksEl.removeChild(villainsRowEl);
   }
-})
+  if (villainsRowEl != tr) {
+    const bid = tr.dataset.bookid;
+    fetch("https://stephen-king-api.onrender.com/api/book/" + bid)
+      .then((res) => res.json())
+      .then((data) => {
+        tr.insertAdjacentHTML(
+          "afterend",
+          `<tr id="villains-row">
+        <td colspan="2">${data.data.Title}</td>
+        <td colspan="4">${data.data.villains
+          .map((villain) => `<a href="${villain.url}">${villain.name}</a>`)
+          .join("<br/>")}</td>
+          </tr>`
+        );
+        const links = Array.from(document.getElementsByTagName("a"));
+        links &&
+          links.forEach((link) => {
+            // console.log(link.href);
+            link.addEventListener("click", (e) => {
+              e.stopPropagation();
+              e.preventDefault();
+              console.log(e.target);
+            });
+          });
+      })
+      .catch((err) => console.log(err));
+  }
+});
